b65d3a314eaae0d92ca05ca7d9b36861587f9649
David Urbano <david.urbano@uao.edu.co>
2024-02-19 07:36:32 -0500

    feat: add pages for auth and middleware

---
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="layout-auth h-full flex items-center justify-center">{children}</div>
+  )
+}
+
+export default AuthLayout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
