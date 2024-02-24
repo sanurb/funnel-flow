994184b6af8d313a554924c9723b94c075b585e3
David Urbano <david.urbano@uao.edu.co>
2024-02-24 00:04:49 -0500

    feat: setup subdomains

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,13 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { dark } from '@clerk/themes'
 import { ThemeProvider } from '@/providers/theme-provider'
-import ModalProvider from '@/providers/modal-provider'
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as SonnarToaster } from '@/components/ui/sonner'
 
 const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Plura',
+  title: 'Funnel Flow',
   description: 'All in one Agency Solution',
 }
 
