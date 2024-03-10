c55e1057449b94034ad965aa8fe31b82affec970
David Urbano <david.urbano@uao.edu.co>
2024-03-10 18:55:19 -0500

    refactor: delete button to use handleDelete function

--- src/app/(main)/agency/[agencyId]/all-subaccounts/_components/delete-button.tsx
+++ src/app/(main)/agency/[agencyId]/all-subaccounts/_components/delete-button.tsx
@@ -14,20 +14,23 @@ type Props = {
 const DeleteButton = ({ subaccountId }: Props) => {
   const router = useRouter()
 
+  const handleDelete = async () => {
+    try {
+      const response = await getSubaccountDetails(subaccountId)
+      await saveActivityLogsNotification({
+        agencyId: undefined,
+        description: `Deleted a subaccount | ${response?.name}`,
+        subaccountId,
+      })
+      await deleteSubAccount(subaccountId)
+      router.refresh()
+    } catch (error) {
+      console.error('Error deleting subaccount:', error)
+    }
+  }
+
   return (
-    <div
-      className="text-white"
-      onClick={async () => {
-        const response = await getSubaccountDetails(subaccountId)
-        await saveActivityLogsNotification({
-          agencyId: undefined,
-          description: `Deleted a subaccount | ${response?.name}`,
-          subaccountId,
-        })
-        await deleteSubAccount(subaccountId)
-        router.refresh()
-      }}
-    >
+    <div className="text-white" onClick={handleDelete}>
       Delete Sub Account
     </div>
   )
