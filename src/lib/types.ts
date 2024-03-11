8d1ec5c5dea8fe79335ea123a8d53238266e25b4
David Urbano <david.urbano@uao.edu.co>
2024-03-11 00:20:22 -0500

    ci: add postinstall script

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,6 @@
 import { Notification, Prisma, Role } from "@prisma/client";
 import { getAuthUserDetails, getUserPermissions } from "./queries";
+import { db } from "./db";
 
 export type NotificationWithUser =
   | ({
@@ -20,5 +21,22 @@ export type UserWithPermissionsAndSubAccounts = Prisma.PromiseReturnType<
   typeof getUserPermissions
 >;
 
+const __getUsersWithAgencySubAccountPermissionsSidebarOptions = async (
+  agencyId: string
+) => {
+  return await db.user.findFirst({
+    where: { Agency: { id: agencyId } },
+    include: {
+      Agency: { include: { SubAccount: true } },
+      Permissions: { include: { SubAccount: true } },
+    },
+  });
+};
+
 export type AuthUserWithAgencySigebarOptionsSubAccounts =
   Prisma.PromiseReturnType<typeof getAuthUserDetails>;
+
+export type UsersWithAgencySubAccountPermissionsSidebarOptions =
+  Prisma.PromiseReturnType<
+    typeof __getUsersWithAgencySubAccountPermissionsSidebarOptions
+  >;
