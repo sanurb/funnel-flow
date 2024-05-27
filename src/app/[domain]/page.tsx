604eed32d7273f5cd8d9151229664b370889ba31
Santiago Urbano <david.urbano@uao.edu.co>
2024-05-27 13:45:17 -0500

    feat: add domain pages

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,5 +1,37 @@
-const page = () => {
-	return <div>Domain</div>;
+import { db } from "@/lib/db";
+import { getDomainContent } from "@/lib/queries";
+import EditorProvider from "@/providers/editor/editor-provider";
+import { notFound } from "next/navigation";
+import FunnelEditor from "../(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor";
+
+const Page = async ({ params }: { params: { domain: string } }) => {
+	const domainData = await getDomainContent(params.domain.slice(0, -1));
+	if (!domainData) return notFound();
+
+	const pageData = domainData.FunnelPages.find((page) => !page.pathName);
+
+	if (!pageData) return notFound();
+
+	await db.funnelPage.update({
+		where: {
+			id: pageData.id,
+		},
+		data: {
+			visits: {
+				increment: 1,
+			},
+		},
+	});
+
+	return (
+		<EditorProvider
+			subaccountId={domainData.subAccountId}
+			pageDetails={pageData}
+			funnelId={domainData.id}
+		>
+			<FunnelEditor funnelPageId={pageData.id} liveMode={true} />
+		</EditorProvider>
+	);
 };
 
-export default page;
+export default Page;
